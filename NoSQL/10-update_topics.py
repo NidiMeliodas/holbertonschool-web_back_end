2864e09bf66513522e2eed4a600f8d62ad646de8
NidiMeliodas <nidigashan@gmail.com>
2024-05-02 13:45:09 +0200

    Task 10 - NoSQL

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Changes all topics of a school document based on the name
+"""
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
+    return mongo_collection.update_many({'name': name},
+                                        {'$set': {'topics': topics}})
