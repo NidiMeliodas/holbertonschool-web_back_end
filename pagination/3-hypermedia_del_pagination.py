758f086fa58c0f2fbb8ce8dd6cbc4b9f8078f87c
NidiMeliodas <nidigashan@gmail.com>
2024-05-03 18:28:36 +0200

    test

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 """
-Implement a get_hyper_index method with two integer arguments: index with a
-None default value and page_size with default value of 10.
-
-The method should return a dictionary with the following key-value pairs:
-index: the current start index of the return page. That is the index of
-the first item in the current page. For example if requesting page 3 with
-page_size 20, and no data was removed from the dataset, the current index
-should be 60.
-next_index: the next index to query with. That should be the index of the
-first item after the last item on the current page.
-page_size: the current page size
-data: the actual page of the dataset
+Deletion-resilient hypermedia pagination
 """
 
 import csv
