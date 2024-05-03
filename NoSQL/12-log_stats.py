7da8ba39bb913b24de41d4175438f6cc7ad460cf
NidiMeliodas <nidigashan@gmail.com>
2024-05-03 15:31:43 +0200

    im fed up

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,19 +10,13 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     """ test"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-
     logs_collection = client.logs.nginx
     total = logs_collection.count_documents({})
     print(f"{total} logs")
-
     print("Methods:")
-
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-
         count = logs_collection.count_documents({"method": method})
-
         print(f"\tmethod {method}: {count}")
-
     status_check = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
