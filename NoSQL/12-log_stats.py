bf4bc7cb2e6f98d8fa0d84f5df2d1687f7dc0ad0
NidiMeliodas <nidigashan@gmail.com>
2024-05-03 16:16:19 +0200

    test

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
-"""Write a Python script that provides some stats
-about Nginx logs stored in MongoDB
 """
+Module: 12-log_stats.py
+Author: TheWatcher01
+Date: 2024-04-29
+Description: Module provides some stats about Nginx logs stored in MongoDB.
 
+Security Note:
+Never embed credentials (like database passwords) directly in your code.
+Instead, use a secure method like environment variables or configuration files
+to keep this information separate from your code.
+"""
 
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
-    """Python script that provides stats about Nginx logs"""
+    # Establish a connection to the MongoDB database
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_db = client.logs
-    nginx_collection = logs_db.nginx
-    x = nginx_collection.count_documents({})
-    print(f"{x} logs")
+
+    # Access the 'nginx' collection in the 'logs' database
+    logs_collection = client.logs.nginx
+
+    # Count the total number of documents in the collection
+    total = logs_collection.count_documents({})
+    print(f"{total} logs")
+
     print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
+    # Iterate over each HTTP method
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        # Use count_documents method of collection to count number of documents
+        # where the 'method' field matches the current HTTP method
+        count = logs_collection.count_documents({"method": method})
+        # Print the HTTP method and the count
         print(f"\tmethod {method}: {count}")
-    count_status = nginx_collection.count_documents(
+
+    # Count the number of documents with the GET method and /status path
+    status_check = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{count_status} status check")
+    print(f"{status_check} status check")
