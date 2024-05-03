e4902ce8ba97b09b576153b3be461e67c721253a
NidiMeliodas <nidigashan@gmail.com>
2024-05-03 15:54:04 +0200

    fix ?

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""
-Task 12 - NoSQL
+"""Write a Python script that provides some stats
+about Nginx logs stored in MongoDB
 """
 
 
@@ -8,15 +8,17 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """ test"""
+    """Python script that provides stats about Nginx logs"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    print(f"{total} logs")
+    logs_db = client.logs
+    nginx_collection = logs_db.nginx
+    x = nginx_collection.count_documents({})
+    print(f"{x} logs")
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = logs_collection.count_documents({"method": method})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-    status_check = logs_collection.count_documents(
+    count_status = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    print(f"{count_status} status check")
