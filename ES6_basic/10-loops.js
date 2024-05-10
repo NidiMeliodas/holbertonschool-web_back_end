89a8b147bc6c0207ece865a3248cb3166aef236f
NidiMeliodas <nidigashan@gmail.com>
2024-05-10 18:09:25 +0200

    Task  10 - ES6_basic

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+	const newArray = [];
+
+	for (const idx of array) {
+		newArray.push(appendString + idx);
+	}
+
+	return newArray;
+}
