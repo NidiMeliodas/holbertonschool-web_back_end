da003e354c469524588af6f141952ece79109a68
NidiMeliodas <nidigashan@gmail.com>
2024-05-11 02:00:16 +0200

    Task 5 - ES6 classes

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,10 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') { throw new Error('Class extending Building must override evacuationWarningMessage'); }
+    this.sqft = sqft;
+  }
+
+  get sqft() { return this._sqft; }
+
+  set sqft(newValue) { this._sqft = newValue; }
+}
