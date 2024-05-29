9fc5439b69eee1bb0a6d60d625acf6b65b5ac5fa
NidiMeliodas <nidigashan@gmail.com>
2024-05-29 22:22:09 +0200

    Task 8 - ES6_data_manipulation

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,23 @@
+// Clean set with strings coincidences
+
+const cleanSet = (set, startString) => {
+  const str = [];
+
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      str.push(item.slice(startString.length));
+    }
+  }
+
+  return str.join('-');
+};
+
+export default cleanSet;
