65624e44605fa6009098a2e762d36d678b8fdd47
NidiMeliodas <nidigashan@gmail.com>
2024-05-29 22:21:20 +0200

    Task 4 - ES6_data_manipulation

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,24 @@
+// Grade by city and add grade
+
+const updateStudentGradeByCity = (students, city, newGrades) => {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+
+  const studentByCity = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grades = newGrades.filter((note) => student.id === note.studentId);
+      let grade = 'N/A';
+
+      if (grades[0]) {
+        grade = grades[0].grade;
+      }
+
+      return { ...student, grade };
+    });
+
+  return studentByCity;
+};
+
+export default updateStudentGradeByCity;
