c390a2bcb957a48190af47cd5b1e4e77011d10c3
NidiMeliodas <nidigashan@gmail.com>
2024-05-29 22:21:48 +0200

    Task 7 - ES6_data_manipulation

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,13 @@
+// Has array values
+
+const hasValuesFromArray = (set, array) => {
+  for (const item of array) {
+    if (!set.has(item)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export default hasValuesFromArray;
