317c3bb03c07a6b268b31970f2da592a498533fc
NidiMeliodas <nidigashan@gmail.com>
2024-05-29 22:22:22 +0200

    Task 9 - ES6_data_manipulation

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+// Groceries List
+
+const groceriesList = () => {
+  const fruits = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  return fruits;
+};
+
+export default groceriesList;
