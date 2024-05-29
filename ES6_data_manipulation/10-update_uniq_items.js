3e9318b80a6494ed3405ca44257119467579b8ec
NidiMeliodas <nidigashan@gmail.com>
2024-05-29 22:22:46 +0200

    Task 10 - ES6_data_manipulation

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,17 @@
+// Update unique items
+
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+};
+
+export default updateUniqueItems;
